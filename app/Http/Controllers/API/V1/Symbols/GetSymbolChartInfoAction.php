98c67c29f016ccaaf622a62da1971300233a4a7c
mohamedyasser <yasoesr@gmail.com>
2025-03-04 21:56:06 +0200

    Update GetSymbolChartInfoAction to handle symbol retrieval for indices

--- app/Http/Controllers/API/V1/Symbols/GetSymbolChartInfoAction.php
+++ app/Http/Controllers/API/V1/Symbols/GetSymbolChartInfoAction.php
@@ -12,8 +12,15 @@ class GetSymbolChartInfoAction
         $symbols = Cache::rememberForever('symbols', function () {
             return Symbols::where('type', 'stock')->get();
         });
-        $symbol = collect($symbols)->keyBy('id')->get($symbol);
-        $url = "https://tvc4.investing.com/" . \Str::uuid()->getHex()->toString() . "/" . time() . "/1/1/8/symbols?symbol={$symbol->inv_id}";
+        $symbolObject = collect($symbols)->keyBy('id')->get($symbol);
+        if ($symbol) {
+            $indices = Cache::rememberForever('indices', function () {
+                return Symbols::where('type', 'index')->get();
+            });
+            $symbolObject = collect($indices)->keyBy('id')->get($symbol);
+        }
+
+        $url = "https://tvc4.investing.com/" . \Str::uuid()->getHex()->toString() . "/" . time() . "/1/1/8/symbols?symbol={$symbolObject->inv_id}";
 
         $res = \Http::baseUrl(config('app.browser_endpoint'))
             ->withBasicAuth(config('app.browser_user_name'), config('app.browser_password'))
