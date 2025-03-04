887b4149b44a82fb9b865156cb6d04b07f1b4a2a
mohamedyasser <yasoesr@gmail.com>
2025-03-05 01:03:43 +0200

    Implement GetSymbolHistoryAction to retrieve symbol history with caching and request parameters

--- app/Http/Controllers/API/V1/Symbols/GetSymbolHistoryAction.php
+++ app/Http/Controllers/API/V1/Symbols/GetSymbolHistoryAction.php
@@ -2,10 +2,36 @@
 
 namespace App\Http\Controllers\API\V1\Symbols;
 
+use App\Models\Symbols;
+use Illuminate\Support\Facades\Cache;
+
 class GetSymbolHistoryAction
 {
-    public function __invoke()
+    public function __invoke($symbol)
     {
-        // TODO: Implement __invoke() method.
+        $symbols = Cache::rememberForever('symbols', function () {
+            return Symbols::where('type', 'stock')->get();
+        });
+        $symbolObject = collect($symbols)->keyBy('id')->get($symbol);
+
+        if (!$symbolObject) {
+            $indices = Cache::rememberForever('indices', function () {
+                return Symbols::where('type', 'index')->get();
+            });
+            $symbolObject = collect($indices)->keyBy('id')->get($symbol);
+        }
+
+        $resolution = request('resolution', 'D');
+        $from = request('from', now()->utc()->startOfDay()->timestamp);
+        $to = request('to', now()->utc()->endOfDay()->timestamp);
+
+        $url = "https://tvc4.investing.com/" . \Str::uuid()->getHex()->toString() . "/" . time() . "/1/1/8/history?symbol={$symbolObject->inv_id}&resolution=$resolution&from=$from&to=$to";
+
+        $res = \Http::baseUrl(config('app.browser_endpoint'))
+            ->withBasicAuth(config('app.browser_user_name'), config('app.browser_password'))
+            ->post('/fetch', [
+                'url' => $url
+            ])->json();
+        return response()->json($res);
     }
 }
