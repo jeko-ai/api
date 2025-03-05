010d748d2054d5e740cef475c677284ba16033d0
mohamedyasser <yasoesr@gmail.com>
2025-03-05 17:55:13 +0200

    Implement GetSymbolHistoryAction to retrieve symbol history with caching and request parameters

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5000', 'https://kira.ws'], // Add your Nuxt frontend URL
+    'allowed_origins' => ['http://localhost:5000', 'https://kira.ws', 'http://localhost:3000'], // Add your Nuxt frontend URL
 
     'allowed_origins_patterns' => [],
 
