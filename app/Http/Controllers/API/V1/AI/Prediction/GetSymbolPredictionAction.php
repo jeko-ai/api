b4e956b12d776c118254c63312c4a4e1a6b22113
mohamedyasser <yasoesr@gmail.com>
2025-03-10 01:49:58 +0200

    a

--- app/Http/Controllers/API/V1/AI/Prediction/GetSymbolPredictionAction.php
+++ app/Http/Controllers/API/V1/AI/Prediction/GetSymbolPredictionAction.php
@@ -3,16 +3,19 @@
 namespace App\Http\Controllers\API\V1\AI\Prediction;
 
 use App\Models\SymbolPricePrediction;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Cache;
 
 class GetSymbolPredictionAction
 {
-    public function __invoke($symbol)
+    public function __invoke($symbol): JsonResponse
     {
-        return Cache::remember("prediction-$symbol", 5 * 60, function () use ($symbol) {
+        $prediction = Cache::remember("prediction-$symbol", 5 * 60, function () use ($symbol) {
             return SymbolPricePrediction::where('symbol_id', $symbol)
                 ->orderByDesc('prediction_date')
                 ->first();
         });
+
+        return response()->json($prediction);
     }
 }
