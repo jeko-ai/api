2da96e548447f20410f5a1b24d0c8da0161e7d8b
mohamedyasser <yasoesr@gmail.com>
2025-03-10 19:51:19 +0200

    Disable timestamps for PortfolioTransactions model to optimize performance

--- app/Models/PortfolioTransactions.php
+++ app/Models/PortfolioTransactions.php
@@ -2,14 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Model;
 
 class PortfolioTransactions extends Model
 {
     use HasUuids;
 
+    public $timestamps = false;
+
     protected $table = 'portfolio_transactions';
+
     protected $fillable = [
         'portfolio_id',
         'symbol_id',
