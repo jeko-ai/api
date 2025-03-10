6671607ce7c69b5a3c2f7f634858f533ca228fa4
mohamedyasser <yasoesr@gmail.com>
2025-03-10 17:24:55 +0200

    Add fillable properties to UserSymbolAlert for enhanced mass assignment

--- app/Models/UserSymbolAlert.php
+++ app/Models/UserSymbolAlert.php
@@ -10,4 +10,23 @@ class UserSymbolAlert extends Model
     use HasUuids;
 
     protected $table = 'user_symbol_alerts';
+
+    protected $fillable = [
+        'user_id',
+        'symbol_id',
+        'inv_id',
+        'tv_id',
+        'enable_price_alert',
+        'price_alert',
+        'new_recommendation',
+        'latest_news',
+        'new_predictions',
+        'unusual_volume_alert',
+        'volatility_alert',
+        'earnings_report_alert',
+        'analyst_rating_alert',
+        'corporate_events_alert',
+        'market_movement_alert',
+        'ai_smart_alert',
+    ];
 }
