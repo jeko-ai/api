183443c1d8cd76172b631d433c987483f5d82e8a
mohamedyasser <yasoesr@gmail.com>
2025-03-11 15:44:53 +0200

    Cache historical stock prices in GetSymbolPricesAction and return JSON response

--- app/Http/Controllers/API/V1/Symbols/GetSymbolPricesAction.php
+++ app/Http/Controllers/API/V1/Symbols/GetSymbolPricesAction.php
@@ -72,16 +72,21 @@ class GetSymbolPricesAction
             return Symbols::where('type', 'stock')->get();
         });
         $symbol = collect($symbols)->keyBy('id')->get($symbol);
-        $time = time();
-        $id = Str::uuid()->getHex()->toString();
 
-        $url = "https://tvc4.investing.com/$id/$time/1/1/8/history?symbol={$symbol->inv_id}&resolution=D&from=$from&to=$to";
+        $prices = Cache::rememberForever("symbol-$symbol->id-prices-$from-$to", function () use ($symbol, $from, $to) {
+            $time = time();
+            $id = Str::uuid()->getHex()->toString();
 
-        $res = Http::baseUrl(config('app.browser_endpoint'))
-            ->withBasicAuth(config('app.browser_user_name'), config('app.browser_password'))
-            ->post('/fetch', [
-                'url' => $url
-            ])->json();
-        return response()->json($res);
+            $url = "https://tvc4.investing.com/$id/$time/1/1/8/history?symbol={$symbol->inv_id}&resolution=D&from=$from&to=$to";
+
+            return Http::baseUrl(config('app.browser_endpoint'))
+                ->withBasicAuth(config('app.browser_user_name'), config('app.browser_password'))
+                ->post('/fetch', [
+                    'url' => $url
+                ])->json();
+        });
+
+
+        return response()->json($prices);
     }
 }
