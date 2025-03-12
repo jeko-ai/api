d73b5f3338fddc91f008558fc968ad68b17764b5
mohamedyasser <yasoesr@gmail.com>
2025-03-12 22:20:33 +0200

    a

--- app/Http/Controllers/API/V1/UpdateUserSettingsAction.php
+++ app/Http/Controllers/API/V1/UpdateUserSettingsAction.php
@@ -9,8 +9,8 @@ class UpdateUserSettingsAction
 {
     public function __invoke(UpdateUserSettingsRequest $request)
     {
-        $profile = Profiles::find('id', $request->attributes->get('user_id'));
-        Profiles::where('id', $request->attributes->get('user_id'))->update($request->validated());
+        $profile = Profiles::find($request->attributes->get('user_id'));
+        $profile->update($request->validated());
 
         return response()->json($profile->refresh());
     }
