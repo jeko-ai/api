de3f9934061681e23604149c9eb93010670eee93
mohamedyasser <yasoesr@gmail.com>
2025-03-12 13:35:09 +0200

    a

--- app/Http/Controllers/API/V1/AI/Prediction/CreatePredictionAction.php
+++ app/Http/Controllers/API/V1/AI/Prediction/CreatePredictionAction.php
@@ -17,15 +17,15 @@ class CreatePredictionAction
         });
         $symbol = collect($symbols)->keyBy('id')->get($request->id);
         $market = $symbol?->market;
-        dd($market->open_at, $market->close_at);
+
         PricePredictionRequests::create([
             'user_id' => $request->attributes->get('user_id'),
             'symbol_id' => $symbol?->id,
             'symbol' => $symbol?->symbol,
             'market_id' => $symbol?->market_id,
             'prediction_type' => $request->prediction_type,
-            'prediction_start_date' => Carbon::parse($request->prediction_start_date)->setTime($market->open_at),
-            'prediction_end_date' => Carbon::parse($request->prediction_end_date)->setTime($market->close_at),
+            'prediction_start_date' => Carbon::parse($request->prediction_start_date)->setTimeFromTimeString($market->open_at),
+            'prediction_end_date' => Carbon::parse($request->prediction_end_date)->setTimeFromTimeString($market->close_at),
         ]);
         return response()->json([]);
     }
