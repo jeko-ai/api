5456fa5751f3d70197e4b893e8f95c72c91b0f0a
mohamedyasser <yasoesr@gmail.com>
2025-03-12 22:17:39 +0200

    a

--- app/Http/Controllers/API/V1/UpdateUserSettingsAction.php
+++ app/Http/Controllers/API/V1/UpdateUserSettingsAction.php
@@ -3,36 +3,15 @@
 namespace App\Http\Controllers\API\V1;
 
 use App\Http\Requests\UpdateUserSettingsRequest;
-use Illuminate\Support\Facades\DB;
+use App\Models\Profiles;
 
 class UpdateUserSettingsAction
 {
     public function __invoke(UpdateUserSettingsRequest $request)
     {
-        $data = DB::selectOne('
-    SELECT * FROM update_user_profile(
-        :full_name,
-        :language,
-        :risk_level,
-        :country_id,
-        :is_notification_enabled,
-        :is_price_alerts_enabled,
-        :is_new_recommendations_alerts_enabled,
-        :is_portfolio_update_alerts_enabled,
-        :is_market_sentiment_alerts_enabled
-    )
-', [
-            'full_name' => $request->input('full_name', null),
-            'language' => $request->input('language', null),
-            'risk_level' => $request->input('risk_level', null),
-            'country_id' => $request->input('country_id', null),
-            'is_notification_enabled' => $request->input('is_notification_enabled', null),
-            'is_price_alerts_enabled' => $request->input('is_price_alerts_enabled', null),
-            'is_new_recommendations_alerts_enabled' => $request->input('is_new_recommendations_alerts_enabled', null),
-            'is_portfolio_update_alerts_enabled' => $request->input('is_portfolio_update_alerts_enabled', null),
-            'is_market_sentiment_alerts_enabled' => $request->input('is_market_sentiment_alerts_enabled', null),
-        ]);
+        $profile = Profiles::find('id', $request->attributes->get('user_id'));
+        Profiles::where('id', $request->attributes->get('user_id'))->update($request->validated());
 
-        return response()->json($data);
+        return response()->json($profile->refresh());
     }
 }
