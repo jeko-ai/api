889750f3dee1f8284048f5a61c4734a9b683228a
mohamedyasser <yasoesr@gmail.com>
2025-03-13 02:46:53 +0200

    a

--- app/Http/Controllers/API/V1/News/GetNewsDetailsAction.php
+++ app/Http/Controllers/API/V1/News/GetNewsDetailsAction.php
@@ -3,15 +3,12 @@
 namespace App\Http\Controllers\API\V1\News;
 
 use App\Models\News;
-use Illuminate\Support\Facades\Cache;
 
 class GetNewsDetailsAction
 {
     public function __invoke(string $slug)
     {
-        $news = Cache::rememberForever($slug, function () use ($slug) {
-            return News::where('slug', $slug)->firstOrFail();
-        });
+        $news = News::where('slug', $slug)->firstOrFail();
 
         return response()->json($news);
     }
