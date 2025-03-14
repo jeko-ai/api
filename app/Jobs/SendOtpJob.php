0c762d019b874af4a8bf8c2563ed12058535fdfa
mohamedyasser <yasoesr@gmail.com>
2025-03-14 23:39:01 +0200

    otp

---
+++ app/Jobs/SendOtpJob.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Jobs;
+
+use Exception;
+use Ichtrojan\Otp\Otp;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Support\Facades\Mail;
+
+class SendOtpJob implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(public string $email)
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     * @throws Exception
+     */
+    public function handle(): void
+    {
+        $otp = (new Otp)->generate($this->email, 'numeric', 6);
+        if ($otp->status) {
+            Mail::send('emails.verify-email', [
+                'otp' => $otp->token,
+                'email' => $this->email
+            ], function ($message) {
+                $message->to($this->email)
+                    ->subject('Verify your email address');
+            });
+        }
+    }
+}
