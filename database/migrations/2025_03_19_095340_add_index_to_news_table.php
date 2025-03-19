1ce709486b1e4483e3a89aee8cd54930e34140e1
mohamedyasser <yasoesr@gmail.com>
2025-03-19 11:55:44 +0200

    Add index on is_rewritten column in news table

---
+++ database/migrations/2025_03_19_095340_add_index_to_news_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('news', function (Blueprint $table) {
+            $table->index('is_rewritten');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('news', function (Blueprint $table) {
+            $table->dropIndex(['is_rewritten']);
+        });
+    }
+};
