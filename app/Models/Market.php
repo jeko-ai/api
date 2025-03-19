83509ea817f2a301b1f1eb3b97b9b025d5477e9d
mohamedyasser <yasoesr@gmail.com>
2025-03-19 11:14:06 +0200

    Add symbols relationship to Market model

--- app/Models/Market.php
+++ app/Models/Market.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Market extends Model
 {
@@ -25,4 +26,9 @@ class Market extends Model
     protected $casts = [
         'trading_days' => 'array'
     ];
+
+    public function symbols(): HasMany
+    {
+        return $this->hasMany(Symbol::class);
+    }
 }
