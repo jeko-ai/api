fec28974b898154a2e575d12f0ca116174bd4a26
mohamedyasser <yasoesr@gmail.com>
2025-04-03 22:58:10 +0200

    a

--- app/Jobs/ProcessNewsNotificationJob.php
+++ app/Jobs/ProcessNewsNotificationJob.php
@@ -3,8 +3,11 @@
 namespace App\Jobs;
 
 use App\Models\News;
+use App\Models\User;
+use App\Notifications\SymbolHasNews;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Support\Facades\Notification;
 
 class ProcessNewsNotificationJob implements ShouldQueue
 {
@@ -23,16 +26,16 @@ class ProcessNewsNotificationJob implements ShouldQueue
      */
     public function handle(): void
     {
-//        $users = User::where([
-//            'is_notification_enabled' => true,
-//            'is_market_sentiment_alerts_enabled' => true
-//        ])->whereHas('symbolAlerts', function ($query) {
-//            $query->where([
-//                'symbol_id' => $this->news->symbol_id,
-//                'latest_news' => true
-//            ]);
-//        })->get();
-//
-//        Notification::send($users, new SymbolHasNews($this->news));
+        $users = User::where([
+            'is_notification_enabled' => true,
+            'is_market_sentiment_alerts_enabled' => true
+        ])->whereHas('symbolAlerts', function ($query) {
+            $query->where([
+                'symbol_id' => $this->news->symbol_id,
+                'latest_news' => true
+            ]);
+        })->get();
+
+        Notification::send($users, new SymbolHasNews($this->news));
     }
 }
