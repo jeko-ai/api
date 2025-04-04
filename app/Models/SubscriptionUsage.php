c8d59c589d9882c02a004c2bfdf7512a97cba26e
mohamedyasser <yasoesr@gmail.com>
2025-04-04 15:03:45 +0200

    a

--- app/Models/SubscriptionUsage.php
+++ app/Models/SubscriptionUsage.php
@@ -2,10 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Laravelcm\Subscriptions\Models\Feature;
 use Laravelcm\Subscriptions\Models\SubscriptionUsage as BaseSubscriptionUsage;
 
 class SubscriptionUsage extends BaseSubscriptionUsage
 {
     use HasUuids;
+
+    public function scopeByFeatureSlug(Builder $builder, string $featureSlug, string|int $planId): Builder
+    {
+        $model = config('laravel-subscriptions.models.feature', Feature::class);
+        $feature = $model::where('plan_id', $planId)->where('slug', $featureSlug)->first();
+
+        return $builder->where('feature_id', $feature ? $feature->getKey() : null);
+    }
 }
