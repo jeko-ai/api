343a3ce9246e66bd2f6a9c59524f07ebd8f813b1
mohamedyasser <yasoesr@gmail.com>
2025-04-06 02:46:56 +0200

    a

---
+++ config/filament-translation-component.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+   "languages" => [
+       "en" => [
+           "label" => "English",
+           "flag" => "us"
+       ],
+       "ar" => [
+           "label" => "Arabic",
+           "flag" => "eg"
+       ],
+   ]
+];
