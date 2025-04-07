0eb97669b0c31b873a8c6bad41bfe42792ae13fd
mohamedyasser <yasoesr@gmail.com>
2025-04-07 22:10:24 +0200

    Add User resource with create, edit, and list pages

---
+++ app/Filament/Admin/Resources/UserResource.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Filament\Admin\Resources;
+
+use App\Models\User;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use TomatoPHP\FilamentTranslationComponent\Components\Translation;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-user';
+    protected static ?int $navigationSort = 2;
+
+    public static function getNavigationGroup(): ?string
+    {
+        return trans('messages.group');
+    }
+
+    public static function getNavigationLabel(): string
+    {
+        return trans('messages.users.title');
+    }
+
+    public static function getPluralLabel(): ?string
+    {
+        return trans('messages.users.title');
+    }
+
+    public static function getLabel(): ?string
+    {
+        return trans('messages.users.title');
+    }
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Section::make()
+                    ->schema([
+                        Translation::make('name')
+                            ->label(trans('messages.users.columns.name'))
+                            ->required(),
+                        
+                        Forms\Components\TextInput::make('email')
+                            ->label(trans('messages.users.columns.email'))
+                            ->email()
+                            ->required()
+                            ->unique(ignoreRecord: true),
+                        
+                        Forms\Components\TextInput::make('password')
+                            ->label(trans('messages.users.columns.password'))
+                            ->password()
+                            ->required()
+                            ->hiddenOn('edit')
+                            ->dehydrated(fn ($state) => filled($state)),
+                    ])->columns(2)
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->label(trans('messages.users.columns.name'))
+                    ->sortable()
+                    ->searchable(),
+
+                Tables\Columns\TextColumn::make('email')
+                    ->label(trans('messages.users.columns.email'))
+                    ->sortable()
+                    ->searchable(),
+
+                Tables\Columns\TextColumn::make('created_at')
+                    ->label(trans('messages.users.columns.created_at'))
+                    ->dateTime()
+                    ->sortable(),
+            ])
+            ->filters([
+                Tables\Filters\TrashedFilter::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => \App\Filament\Admin\Resources\UserResource\Pages\ListUsers::route('/'),
+            'create' => \App\Filament\Admin\Resources\UserResource\Pages\CreateUser::route('/create'),
+            'edit' => \App\Filament\Admin\Resources\UserResource\Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+}
