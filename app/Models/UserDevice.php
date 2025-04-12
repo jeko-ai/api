9cf288738da59d6326ee398fbea6f1b27cc0b3ce
mohamedyasser <yasoesr@gmail.com>
2025-04-12 12:57:27 +0200

    Add HasUuids trait to UserDevice model for UUID support

--- app/Models/UserDevice.php
+++ app/Models/UserDevice.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 
 class UserDevice extends Model
 {
+    use HasUuids;
+
     protected $fillable = [
         'fcm_token',
         'user_id',
