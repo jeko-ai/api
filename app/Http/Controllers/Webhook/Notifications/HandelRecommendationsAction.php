9cd931144b7658e3caa37b2ebcf8477428916ee5
mohamedyasser <yasoesr@gmail.com>
2025-04-14 11:52:37 +0200

    a

--- app/Http/Controllers/Webhook/Notifications/HandelRecommendationsAction.php
+++ app/Http/Controllers/Webhook/Notifications/HandelRecommendationsAction.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers\Webhook\Notifications;
 
 use App\Jobs\ProcessRecommendationsNotificationJob;
+use App\Models\Recommendation;
 use F9Web\ApiResponseHelpers;
 
 class HandelRecommendationsAction
 {
     use ApiResponseHelpers;
 
-    public function __invoke($id)
+    public function __invoke(Recommendation $id)
     {
         ProcessRecommendationsNotificationJob::dispatch($id);
 
